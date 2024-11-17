ddc30ec16333d58f9a148cecf08e06e14bc5e366
eslam hassan <hassaneslam612@gmail.com>
2024-11-17 23:02:57 +0200

    Add project files.

---
+++ Part 01 Generics - SWAP Example/Helper.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_01_Generics___SWAP_Example
+{
+    internal class Helper<T> where  T : IComparable<T> , new()
+    {
+        //generic method
+        public static void SWAP(ref T x , ref T y)
+        {
+            //Console.WriteLine("============ SWAPING");
+            T temp = x; 
+            x = y;
+            y = temp;
+        }
+        public static int SearchArray(T[] arr, T value)
+        {
+            for (int i = 0; i < arr?.Length; i++)
+                if(value.Equals(arr[i]))
+                    return i;
+            
+             return -1;
+            
+        }
+        public  static void BubbleSort(T[]arr)
+        {
+            for(int i = 0;  i < arr?.Length; i++)
+            {
+                for (int j = 0; j < arr.Length -i -1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j+1])==1)
+                        SWAP(ref arr[j], ref arr[j+1]);
+                }
+            }
+        }
+
+    }
+    public class Employee :  IComparable<Employee> 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee other)
+            {
+                return this.Id == other.Id && this.Name == other.Name;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id}:: {this.Name} :: {this.Salary}";
+        }
+
+        ///========================================= non-generic 
+        //public int CompareTo(object? obj)
+        //{
+
+        //    #region is Casting Operator 
+        //    //Employee employee2 = (Employee)obj; // unsafe Casting 
+        //    /**using is Conditional Operator:
+        //     return True in 3 Cases:
+        //    1.obj is Employee 
+        //    2.obj object form class Inhert from employee
+        //    3.is null
+        //     **/
+        //    //if (obj is Employee employee2)
+        //    //{
+        //    //    if(obj is null) return 1;
+        //    //    if (this.Salary > employee2.Salary)
+        //    //        return 1;
+        //    //    else if (this.Salary < employee2.Salary)
+        //    //        return -1;
+
+        //    //}
+        //    #endregion
+
+        //    #region as Casting Operator
+        //    // If Casting failed, return null, No Exception Will be throw   
+        //    Employee employee2 = obj as Employee;
+        //    if (employee2 == null) return 0;
+        //    if (this.Salary > employee2.Salary)
+        //        return 1;
+        //    else if (this.Salary < employee2.Salary)
+        //        return -1;
+        //    return 1;
+        //    #endregion
+        //}
+        ///================================================
+       ///================================ generic 
+        public int CompareTo(Employee? employee2)
+        {
+
+            if (this.Salary > employee2.Salary)
+                return 1;
+            else if (this.Salary < employee2.Salary)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    public struct Employee2 : IComparable<Employee2>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Employee2(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee other)
+            {
+                return this.Id == other.Id && this.Name == other.Name;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"{this.Id}:: {this.Name} :: {this.Salary}";
+        }
+
+        public int CompareTo(Employee2 employee2)
+        {
+            
+            if (this.Salary > employee2.Salary)
+                return 1;
+            else if (this.Salary < employee2.Salary)
+                return -1;
+            else
+            return 0;
+        }
+    }
+}
---
+++ Part 01 Generics - SWAP Example/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Part_01_Generics___SWAP_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Generic SWAP
+
+
+            //int x=15 ,y=10 ;
+            //Console.WriteLine($"X= {x}");
+            //Console.WriteLine($"Y= {y}");
+            //// complier can be detect the type T
+            //Helper.SWAP(ref x,ref y);
+            //Console.WriteLine($"X= {x}");
+            //Console.WriteLine($"Y= {y}");
+            #endregion
+            #region Search
+            //Employee employee01 = new Employee(1, "eslam", 20_000);
+            //Employee employee02 = new Employee(2, "ahmed", 25_000);
+
+            //Employee[] employes = { employee01, employee02  , new Employee(3, "ali", 200) };
+
+            ////int result =  Helper<Employee>.SearchArray(employes, new Employee(2, "ahmed", 25_000));
+            //Console.WriteLine(result);
+            #endregion
+            #region Sort
+            // Struct
+            Employee2[] employee2 =
+            {
+                new Employee2(1, "test1" , 170),
+                new Employee2(2, "test2" , 200),
+                new Employee2(3, "test3" , 150),
+                new Employee2(4, "test4" , 190),
+            };
+            Helper<Employee2>.BubbleSort(employee2);
+
+            //Class
+            Employee[] employee =
+          {
+                new Employee(1, "ahmed" , 170),
+                new Employee(2, "eslam" , 200),
+                new Employee(3, "eee" , 150),
+                new Employee(4, "tttt" , 190),
+            };
+            // base Class
+            Helper<Employee>.BubbleSort(employee);
+            // base Struct
+            Helper<Employee2>.BubbleSort(employee2);
+
+
+            foreach (var item in employee)
+                Console.WriteLine(item);
+
+            Console.WriteLine("================================");
+            foreach (var item in employee2)
+                Console.WriteLine(item);
+            #endregion
+
+        }
+        
+    }
+}
